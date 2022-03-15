f6bd49b9efcd24cff39aedd94862c47562ddcb82
dunglv <dunglv3@itsol.vn>
2022-03-15 15:30:29 +0700

    config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./dist/front_booking_room"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/front_booking_room" });
+  res.sendFile("index.html", { root: "./dist/front_booking_room" });
 });
 
 app.listen(process.env.PORT || 8080);
